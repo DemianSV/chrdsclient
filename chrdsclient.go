eba5be10903f9feaf7102e6a9be8e410615bf11e
Dmitry Sovetin <sovetin@gmail.com>
2024-09-18 17:07:14 +0300

    Connect to multiple DataManager nodes

--- chrdsclient.go
+++ chrdsclient.go
@@ -1,4 +1,4 @@
-// Charybdis Monitoring System client v0.0.2
+// Charybdis Monitoring System client v1.0.1
 
 package chrdsclient
 
@@ -7,7 +7,6 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"errors"
-	"log"
 	"net/http"
 	"strings"
 	"time"
@@ -16,11 +15,25 @@ import (
 type ConfT struct {
 	ClientInSecureSkipVerify bool
 	SpaceID                  string
-	ModulID                  string
-	DataManagerURL           string
+	ModuleID                 string
+	DataManagerURL           []string
 }
 
 var Conf ConfT
+
+func statusCheck() (int, bool) {
+	var index int
+	status := Status()
+	availability := false
+	for i, item := range status {
+		if item {
+			index = i
+			availability = true
+			break
+		}
+	}
+	return index, availability
+}
 
 // Логирование в систему CHRDS
 func Log(metric string, value string) error {
@@ -45,71 +58,75 @@ func Log(metric string, value string) error {
 	requestRawTextSave.Object = "uimanager"
 	requestRawTextSaveA.Data = append(requestRawTextSaveA.Data, requestRawTextSave)
 
-	if len(requestRawTextSaveA.Data) > 0 {
-		requestRawTextSaveJSON, err := json.Marshal(requestRawTextSaveA)
-		if err != nil {
-			log.Print("JSON MARSHAL ERROR (", err, ")")
-			return errors.New("JSON MARSHAL ERROR (" + err.Error() + ")")
-		} else {
-			req, err := http.NewRequest("POST", Conf.DataManagerURL+"/api/v1/text/save", bytes.NewBuffer(requestRawTextSaveJSON))
+	index, availability := statusCheck()
+
+	if availability {
+		if len(requestRawTextSaveA.Data) > 0 {
+			requestRawTextSaveJSON, err := json.Marshal(requestRawTextSaveA)
 			if err != nil {
-				return errors.New("HTTP REQUEST ERROR (" + err.Error() + ")")
+				return errors.New("JSON MARSHAL ERROR (" + err.Error() + ")")
+			} else {
+				req, err := http.NewRequest("POST", Conf.DataManagerURL[index]+"/api/v1/text/save", bytes.NewBuffer(requestRawTextSaveJSON))
+				if err != nil {
+					return errors.New("HTTP REQUEST ERROR (" + err.Error() + ")")
+				}
+				req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+				req.Header.Set("api_key", Conf.ModuleID) // Модуль UIManager
+
+				client := &http.Client{}
+				if strings.Contains(strings.ToUpper(Conf.DataManagerURL[index]), strings.ToUpper("https:")) {
+					transport := &http.Transport{
+						TLSClientConfig: &tls.Config{InsecureSkipVerify: Conf.ClientInSecureSkipVerify},
+					}
+					client = &http.Client{Transport: transport}
+				}
+				resp, err := client.Do(req)
+				if err != nil {
+					return errors.New("HTTP REQUEST ERROR (" + err.Error() + ")")
+				}
+				defer resp.Body.Close()
+
+				return nil
 			}
+		} else {
+			return errors.New("NO DATA TO SEND")
+		}
+	}
+	return errors.New("DATAMANAGER IS NOT AVAILABLE")
+}
+
+func Status() []bool {
+	var responseUp []bool
+
+	for _, item := range Conf.DataManagerURL {
+		req, err := http.NewRequest("GET", item+"/api/v1/version", nil)
+		if err != nil {
+			responseUp = append(responseUp, false)
+		} else {
 			req.Header.Set("Content-Type", "application/json; charset=UTF-8")
-			req.Header.Set("api_key", Conf.ModulID) // Модуль UIManager
+			req.Header.Set("api_key", Conf.ModuleID) // Модуль UIManager
 
 			client := &http.Client{}
-			if strings.Contains(strings.ToUpper(Conf.DataManagerURL), strings.ToUpper("https:")) {
+			if strings.Contains(strings.ToUpper(item), strings.ToUpper("https:")) {
 				transport := &http.Transport{
 					TLSClientConfig: &tls.Config{InsecureSkipVerify: Conf.ClientInSecureSkipVerify},
 				}
-				client = &http.Client{Transport: transport}
+				client = &http.Client{Transport: transport, Timeout: 3 * time.Second}
 			}
 			resp, err := client.Do(req)
 			if err != nil {
-				return errors.New("HTTP REQUEST ERROR (" + err.Error() + ")")
-			}
-			defer resp.Body.Close()
-
-			return nil
-		}
-	} else {
-		return errors.New("NO DATA TO SEND")
-	}
-}
-
-func Status() ([]bool, error) {
-	var responseUp []bool
-
-	req, err := http.NewRequest("GET", Conf.DataManagerURL+"/api/v1/version", nil)
-	if err != nil {
-		responseUp = append(responseUp, false)
-		return responseUp, errors.New("HTTP REQUEST ERROR (" + err.Error() + ")")
-	} else {
-		req.Header.Set("Content-Type", "application/json; charset=UTF-8")
-		req.Header.Set("api_key", Conf.ModulID) // Модуль UIManager
-
-		client := &http.Client{}
-		if strings.Contains(strings.ToUpper(Conf.DataManagerURL), strings.ToUpper("https:")) {
-			transport := &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: Conf.ClientInSecureSkipVerify},
-			}
-			client = &http.Client{Transport: transport}
-		}
-		resp, err := client.Do(req)
-		if err != nil {
-			responseUp = append(responseUp, false)
-			return responseUp, errors.New("HTTP REQUEST ERROR (" + err.Error() + ")")
-		} else {
-			if resp.StatusCode == 200 {
-				responseUp = append(responseUp, true)
-			} else {
 				responseUp = append(responseUp, false)
+			} else {
+				if resp.StatusCode == 200 {
+					responseUp = append(responseUp, true)
+				} else {
+					responseUp = append(responseUp, false)
+				}
 			}
 		}
 	}
 
-	return responseUp, nil
+	return responseUp
 }
 
 func MakeTimestamp() int64 {
@@ -138,34 +155,39 @@ func Metric(metric string, value float32) error {
 	requestRawDataSave.Object = "uimanager"
 	requestRawDataSaveArray.Data = append(requestRawDataSaveArray.Data, requestRawDataSave)
 
-	if len(requestRawDataSaveArray.Data) > 0 {
-		requestRawDataSaveJSON, err := json.Marshal(requestRawDataSaveArray)
-		if err != nil {
-			return errors.New("JSON MARSHAL ERROR (" + err.Error() + ")")
-		} else {
-			req, err := http.NewRequest("POST", Conf.DataManagerURL+"/api/v1/data/save", bytes.NewBuffer(requestRawDataSaveJSON))
-			if err != nil {
-				log.Print("HTTP REQUEST ERROR (", err, ")")
-			}
-			req.Header.Set("Content-Type", "application/json; charset=UTF-8")
-			req.Header.Set("api_key", Conf.ModulID) // Модуль UIManager
+	index, availability := statusCheck()
 
-			client := &http.Client{}
-			if strings.Contains(strings.ToUpper(Conf.DataManagerURL), strings.ToUpper("https:")) {
-				transport := &http.Transport{
-					TLSClientConfig: &tls.Config{InsecureSkipVerify: Conf.ClientInSecureSkipVerify},
+	if availability {
+		if len(requestRawDataSaveArray.Data) > 0 {
+			requestRawDataSaveJSON, err := json.Marshal(requestRawDataSaveArray)
+			if err != nil {
+				return errors.New("JSON MARSHAL ERROR (" + err.Error() + ")")
+			} else {
+				req, err := http.NewRequest("POST", Conf.DataManagerURL[index]+"/api/v1/data/save", bytes.NewBuffer(requestRawDataSaveJSON))
+				if err != nil {
+					return errors.New("HTTP REQUEST ERROR (" + err.Error() + ")")
 				}
-				client = &http.Client{Transport: transport}
-			}
-			resp, err := client.Do(req)
-			if err != nil {
-				return errors.New("HTTP REQUEST ERROR (" + err.Error() + ")")
-			}
-			defer resp.Body.Close()
+				req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+				req.Header.Set("api_key", Conf.ModuleID) // Модуль UIManager
 
-			return nil
+				client := &http.Client{}
+				if strings.Contains(strings.ToUpper(Conf.DataManagerURL[index]), strings.ToUpper("https:")) {
+					transport := &http.Transport{
+						TLSClientConfig: &tls.Config{InsecureSkipVerify: Conf.ClientInSecureSkipVerify},
+					}
+					client = &http.Client{Transport: transport}
+				}
+				resp, err := client.Do(req)
+				if err != nil {
+					return errors.New("HTTP REQUEST ERROR (" + err.Error() + ")")
+				}
+				defer resp.Body.Close()
+
+				return nil
+			}
+		} else {
+			return errors.New("NO DATA TO SEND")
 		}
-	} else {
-		return errors.New("NO DATA TO SEND")
 	}
+	return errors.New("DATAMANAGER IS NOT AVAILABLE")
 }
